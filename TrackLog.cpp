b1e6a5ffafb8b24e1d26e3b1aa10979c420e0423
arnoldqixue <82875596+arnoldqixue@users.noreply.github.com>
2021-12-15 15:59:06 +0800

    Update TrackLog.cpp

--- TrackLog.cpp
+++ TrackLog.cpp
@@ -1,6 +1,6 @@
 #include "TrackLog.h"
 void LogInfo(char* File, char* Funtion, int lineNum){
-	ofstream out("txt.log", ios::app | ios::out);
+	ofstream out("TrackLog.log", ios::app | ios::out);
 	if (out.is_open())
 	{
 		out << File << "->" << Funtion << "->" << lineNum << "(" << __TIME__<<")\n";
